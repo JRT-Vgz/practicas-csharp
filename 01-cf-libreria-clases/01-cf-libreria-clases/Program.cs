dcc393af4e84d960346278cc4714ba36cbcfa125
Vargath <jrtvgz@gmail.com>
2024-07-16 18:50:45 +0200

    demo librerias de clases c#

---
+++ 01-cf-libreria-clases/01-cf-libreria-clases/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using libClientes;
+
+clsClientes cliente;
+cliente = new clsClientes();
+
+cliente.idCliente = 1;
+cliente.NombreCompleto = "Xaxi Pistachi";
+cliente.Rfc = "HEHM010165";
+
+Console.WriteLine($"cliente.idCliente tiene valor {cliente.idCliente} y es de tipo {cliente.idCliente.GetType()}.");
+Console.WriteLine($"cliente.NombreCompleto tiene valor {cliente.NombreCompleto} y es de tipo {cliente.NombreCompleto.GetType()}.");
+Console.WriteLine($"cliente.Rfc tiene valor {cliente.Rfc} y es de tipo {cliente.Rfc.GetType()}.");
+Console.WriteLine(cliente.ToString());
